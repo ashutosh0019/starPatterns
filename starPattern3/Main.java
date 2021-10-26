b9f5fac4313b28f8d7655c396c0b9f30ad48277d
ashutosh0019 <ashutoshkr0195@gmail.com>
2021-10-27 00:34:26 +0530

    opposite triangle pattern

--- starPattern3/Main.java
+++ starPattern3/Main.java
@@ -1,17 +1,19 @@
-package starPattern1;
-public class Pattern1 {
+package starPattern3;
+
+public class Main {
     public static void main(String[] args) {
-       int n=5;
-       printStar(n);
+        int n=5;
+        printStar(n);
     }
     public static void printStar(int n){
         for (int row=1; row<= n; row++){
             //for every row run the column
-            for (int col= 1; col<=row; col++){
+            for (int col= 1; col<= n-row+1; col++){
                 System.out.print("* ");
             }
             //when row is printed, we need to add a new line
             System.out.println();
         }
     }
+    
 }
