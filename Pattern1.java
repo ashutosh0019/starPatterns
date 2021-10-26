7a85b5b94abe5f54e9b0698622c6f79d7b35db10
ashutosh0019 <ashutoshkr0195@gmail.com>
2021-10-27 00:09:57 +0530

    first star pattern

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+package javaPatterns;
+public class Pattern1 {
+    public static void main(String[] args) {
+       int n=5;
+       printStar(n);
+    }
+    public static void printStar(int n){
+        for (int row=1; row<= n; row++){
+            //for every row run the column
+            for (int col= 1; col<=row; col++){
+                System.out.print("* ");
+            }
+            //when row is printed, we need to add a new line
+            System.out.println();
+        }
+    }
+}
